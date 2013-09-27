9aea32c772764c75f3dcc75068917197c49d17ec
Gregory Burd <greg@basho.com>
2013-09-27 10:28:49 -0400

    DPRINTF macro lives inside of lmdb code, remove it from common header.

--- c_src/common.h
+++ c_src/common.h
@@ -24,24 +24,6 @@
 extern "C" {
 #endif
 
-#if !(__STDC_VERSION__ >= 199901L || defined(__GNUC__))
-# undef  DEBUG
-# define DEBUG		0
-# define DPRINTF	(void)	/* Vararg macros may be unsupported */
-#elif DEBUG
-#include <stdio.h>
-#include <stdarg.h>
-#define DPRINTF(fmt, ...)							\
-    do {									\
-	fprintf(stderr, "%s:%d " fmt "\n", __FILE__, __LINE__, __VA_ARGS__);    \
-	fflush(stderr);								\
-    } while(0)
-#define DPUTS(arg)	DPRINTF("%s", arg)
-#else
-#define DPRINTF(fmt, ...)	((void) 0)
-#define DPUTS(arg)		((void) 0)
-#endif
-
 #ifndef UNUSED
 #define UNUSED(v) ((void)(v))
 #endif
